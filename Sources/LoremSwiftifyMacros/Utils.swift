2267cd7083839b4f06cc16834462768ac6c6a73e
Enes Karaosman <eneskaraosman53@gmail.com>
2024-03-25 17:41:35 +0300

    Update Utils.swift

--- Sources/LoremSwiftifyMacros/Utils.swift
+++ Sources/LoremSwiftifyMacros/Utils.swift
@@ -12,7 +12,23 @@ extension MemberBlockItemListSyntax {
         first(where: { $0.decl.is(InitializerDeclSyntax.self) })?.decl.as(InitializerDeclSyntax.self)
     }
 
+    var structDeclSyntax: StructDeclSyntax? {
+        first(where: { $0.decl.is(StructDeclSyntax.self) })?.decl.as(StructDeclSyntax.self)
+    }
+
+    var classDeclSyntax: ClassDeclSyntax? {
+        first(where: { $0.decl.is(ClassDeclSyntax.self) })?.decl.as(ClassDeclSyntax.self)
+    }
+
     var hasInit: Bool {
         initializerDeclSyntax != nil
+    }
+
+    var hasStructMember: Bool {
+        structDeclSyntax != nil
+    }
+
+    var hasClassMember: Bool {
+        classDeclSyntax != nil
     }
 }
